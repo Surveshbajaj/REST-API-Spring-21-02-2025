7f51fb225929680bf8f2ef5ab492bc7fed659237
SurveshBajaj <Sarveshbajaj9@gmail.com>
2025-02-21 22:35:56 +0530

    [Survesh Bajaj]- Add. Create a Rest Controller to demonstrate

---
+++ Rest-api-call/hellofrombridgelabz/hellofrombridgelabz/src/main/java/com/bridgelabz/hellofrombridgelabz/HellofrombridgelabzApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellofrombridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellofrombridgelabzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellofrombridgelabzApplication.class, args);
+	}
+
+}
---
+++ Rest-api-call/hellofrombridgelabz/hellofrombridgelabz/src/test/java/com/bridgelabz/hellofrombridgelabz/HellofrombridgelabzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellofrombridgelabz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellofrombridgelabzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
